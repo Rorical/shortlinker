da6ab90a58cf1588b800afecb4ea0738f81a9394
Rorical <46294886+Rorical@users.noreply.github.com>
2022-03-03 17:23:48 +0800

    referral

--- src/views/About.vue
+++ src/views/About.vue
@@ -6,9 +6,12 @@
 		超级简陋，也不准备加什么东西，唯一的目的是用来A*** C*********
 		
 		<br>
+		<br>
 		此站采用hCaptcha并遵循其
 		<a href="https://hcaptcha.com/privacy">隐私政策</a> 和
 		<a href="https://hcaptcha.com/terms">服务条款</a> .
+		<br>
+		来用hCaptcha保护你的站点吧! <a href="https://hCaptcha.com/?r=9ffe8d3ee0e2" target="_blank">点我注册hCaptcha</a>
 	</el-main>
 </template>
 
